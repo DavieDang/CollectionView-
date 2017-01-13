047cef15c34231dfa412160fd67eca23dff385aa
admin <dangwc@gz-mstc.com>
2017-01-13 17:53:14 +0800

    demodoudong
    demodoudong

---
+++ CollectionView抖动后拖拽/VibrateCollectionViewCell.h
@@ -0,0 +1,32 @@
+//
+//  VibrateCollectionViewCell.h
+//  CollectionView抖动后拖拽
+//
+//  Created by songshu on 16/9/9.
+//  Copyright © 2016年 SongShu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol VibrateCollectionViewCellDelegate <NSObject>
+
+- (void)clickdelected:(UIButton *)sender;
+
+@end
+
+
+@interface VibrateCollectionViewCell : UICollectionViewCell
+
+//@property (nonatomic,copy) NSString* nameLableStr;
+//@property (nonatomic,copy) NSString* imageStr;
+
+@property (nonatomic,strong) UIImageView* headImageView;
+@property (nonatomic,strong) UILabel* nameLable;
+
+@property (nonatomic,strong) UIButton *delectedBtn;
+
+@property (nonatomic,assign) id<VibrateCollectionViewCellDelegate> delegate;
+
+
+
+@end
